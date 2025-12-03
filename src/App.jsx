2f272880298a385dd9788efcee0e29793badc356
Ammar Hanafi <148710609+ammarhnfi@users.noreply.github.com>
2025-12-03 14:55:14 +0700

    Update siteProps with new user information

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Ammar Hanafi",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  email: "ammar@gmail.com",
+  gitHub: "github.com/ammarhnfi",
+  instagram: "instagram.com/ammarhnfi",
+  linkedIn: "linkedin.com/in/ammarhnfi",
+  medium: "medium.com/@ammarhnfi",
+  x: "x.com/ammarhnfi",
+  youTube: "Ammar Hanafi",
 };
 
 const primaryColor = "#4E567E";
